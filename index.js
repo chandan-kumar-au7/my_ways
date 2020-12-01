91b6bdb26296f8131fd6c3d35dce45ce0ae298aa
chandan-kumar-au7 <chandankr.pra930@gmail.com>
2020-12-01 13:33:22 +0530

    new push solving some bug

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./views/build"));
+  app.use(express.static("./client/build"));
 } else {
   // IN DEVLOPMENT MODE ONLY
   app.use(morgan("dev"));
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 app.use("/user", userAuthRoute);
 
 app.get("/*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "/views/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
   next();
 });
 
